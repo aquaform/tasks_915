2ee7f81db0d1a0fc7c4a2df8c78d1794ec23f638
Vadim Slyvka <vadik-slyka@yandex.ru>
2020-12-01 21:25:09 +0500

    modified_8

---
+++ Слывка/Задание 2/js/index.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    var x = document.getElementById("answer_1");
+        if (x.style.display === "none") {
+            x.style.display = "block";
+        } else {
+            x.style.display = "none";
+        }
+    }
+
+(function () {
+    const header = document.querySelector('.header');
+    window.onscroll = () => {
+        if (window.pageYOffset > 200) {
+            header.classList.add('header_active');
+        } else {
+            header.classList.remove7('header_active'); 
+        }
+    }
+}());
